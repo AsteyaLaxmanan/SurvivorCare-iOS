321554b8c068eb897ae09a45f03c231ce1fc777b
Asteya Laxmanan <asktheduck123@gmail.com>
2025-11-08 21:16:57 -0600

    Initial Commit

---
+++ SurvivorshipCareSwift/SurvivorshipCareSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurvivorshipCareSwiftApp.swift
+//  SurvivorshipCareSwift
+//
+//  Created by Asteya Laxmanan on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SurvivorshipCareSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SurvivorshipCareSwiftTests/SurvivorshipCareSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  SurvivorshipCareSwiftTests.swift
+//  SurvivorshipCareSwiftTests
+//
+//  Created by Asteya Laxmanan on 11/8/25.
+//
+
+import Testing
+@testable import SurvivorshipCareSwift
+
+struct SurvivorshipCareSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
